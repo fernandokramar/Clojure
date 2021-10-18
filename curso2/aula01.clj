52383cf8287db942a8558c72c8d4402b6b1940b4
Fernando Macedo Kramar <macedokramar@gmail.com>
2021-10-18 17:22:44 -0300

    novo curso segundatech

---
+++ curso2/aula01.clj
@@ -0,0 +1,9 @@
+;REPL(Read Eval Print Loop)
+
+["marcio" "frayze" 39 3.14]
+
+; conta a quantidade de elementos 
+(count [1 2 3 4])
+
+; adiciona um valor no vetor (primeiro vetor depois o elemento que quero colocar no vetor)
+(conj [1 2 3 4] 5)
